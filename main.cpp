0557dc6fc29f1cc00c3591a8064bb1f5edfa63e3
asssh52 <aptemka9000@gmail.com>
2024-09-08 19:36:35 +0300

    v4 fread

--- main.cpp
+++ main.cpp
@@ -9,7 +9,8 @@
                                             __LINE__, __FILE__, __func__); abort();}
 enum sizes{
     STR_LENGTH = 60,
-    STR_NUM = 8
+    STR_NUM = 8,
+    FILE_SIZE = 600
 };
 
 
@@ -63,6 +64,20 @@ void BubbleSortStrings(char** sourceText){
     }
 }
 
+void SplitText(char* buffer, char** storage){
+    int lineCounter = 0;
+    *storage = buffer; // mem start
+
+    for (int i = 0; buffer[i] != EOF; i++){
+        if (buffer[i] == '\n'){
+            lineCounter++;
+            printf("%lu ", (size_t)(buffer + i));
+            *(storage + lineCounter) = (char*)((size_t)buffer + i);
+        }
+    }
+    //printf("%d\n", lineCounter);
+}
+
 void PrintTextDebug(const char** sourceText){
     for (int i = 0; i < STR_NUM; i++){
         for (int j = 0; j < STR_LENGTH; j++){
@@ -83,7 +98,7 @@ void PrintText(const char** sourceText){
 
 int main(){
 
-    char sourceFile[] = "onegin.txt";
+    const char* sourceFile = "onegin.txt";
     FILE* sourceFileLink = fopen(sourceFile, "r");
 
     char sourceOnegin[STR_NUM][STR_LENGTH] = {      {    "``My uncle -- high ideals inspire him;"   },
@@ -101,8 +116,28 @@ int main(){
         sourcePointer[i] = sourceOnegin[i];
     }
 
-    BubbleSortStrings(sourcePointer);
-    PrintText((const char**)sourcePointer);
+    void* bufferPointer = calloc(FILE_SIZE, sizeof(char));
+    char** textPointer = (char**)calloc(STR_NUM + 1, sizeof(char*)); // что здесь происходит???
 
+    ASSERT(sourceFileLink != NULL && bufferPointer != NULL);
+    fread(bufferPointer, sizeof(char), FILE_SIZE, sourceFileLink);
+
+    SplitText((char*)bufferPointer, (char**)textPointer);
+
+    printf("\n\n");
+    for (int i = 0; i < STR_NUM; i++){
+        printf("%lu \n", *(textPointer + i));
+    }
+
+    printf("%d\n", *(textPointer + 2) - *(textPointer + 0));
+
+    for (int i = 0; i < *(textPointer + 2) - *(textPointer + 0); i++){
+        printf("%c'%d'", *(char*)((size_t)bufferPointer + i), *(char*)((size_t)bufferPointer + i));
+    }
+
+    //BubbleSortStrings(sourcePointer);
+    //PrintText((const char**)sourcePointer);
+
+    //создать "массив чтения" || менять указатели на строки
     return 0;
 }
