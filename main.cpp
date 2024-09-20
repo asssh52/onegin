9e0966c6d2cde69bd53b16cf6c42204eb9b0d027
asssh52 <aptemka9000@gmail.com>
2024-09-20 15:19:20 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,8 +3,6 @@
 #include "structure_text.hpp"
 #include "myassert.hpp"
 
-
-
 int main(int argc, const char* argv[]){
     const char* filenameIn  = (argc == 2)? argv[1] : "in_onegin.txt";
     const char* filenameOut = (argc == 3)? argv[2] : "out_onegin.txt";
